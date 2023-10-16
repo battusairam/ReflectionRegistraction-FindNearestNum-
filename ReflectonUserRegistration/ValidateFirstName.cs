ec22dfa4ee8f1df761d8901139b8eadd2c75aef3
“battusairam” <battusairam6@gmail.com>
2023-10-16 21:20:49 +0530

    [SAIRAM] Reflection User_Registraction And Find Nearest Number

---
+++ ReflectonUserRegistration/ValidateFirstName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReflectonUserRegistration
+{
+    internal class ValidateFirstName
+    {
+        public static bool Validate_FirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, "^[A-Z][a-z]{2,}");
+        }
+    }
+}
